8913e9678eef7bf97ad4b7299e630b25b9e8e000
xdavidwu <xdavidwuph@gmail.com>
2023-07-18 16:43:59 +0800

    code refactor

--- qt-video-wlr-bg.cpp
+++ qt-video-wlr-bg.cpp
@@ -1,7 +1,6 @@
 #include <QUrl>
 #include <QWidget>
 #include <QFileInfo>
-#include <QPushButton>
 #include <QApplication>
 #include <QMediaPlayer>
 #include <QVideoWidget>
@@ -11,66 +10,24 @@
 #include <iostream>
 #include "wlr-layer-shell-unstable-v1-client-protocol.h"
 
-static struct zwlr_layer_shell_v1 *layer_shell;
-struct zwlr_layer_surface_v1 *layer_surface;
-static struct wl_output *wl_output;
-struct wl_surface *wl_surface;
-
-uint32_t width = 320, height = 240;
-
-QApplication *app;
-QWidget *root;
-QVideoWidget *videoWidget;
-
-static void handle_global(void *data, struct wl_registry *registry,
-		uint32_t name, const char *interface, uint32_t version) {
-	if (strcmp(interface, zwlr_layer_shell_v1_interface.name) == 0) {
-		layer_shell = (zwlr_layer_shell_v1*)wl_registry_bind(
-				registry, name, &zwlr_layer_shell_v1_interface, 1);
-	}
-}
-
-static void handle_global_remove(void *data, struct wl_registry *registry,
-		uint32_t name) {
-	// who cares
-}
-
-static const struct wl_registry_listener registry_listener = {
-	.global = handle_global,
-	.global_remove = handle_global_remove,
+struct qvw_state {
+	struct zwlr_layer_shell_v1 *layer_shell;
+	uint32_t width, height;
+	QApplication *app;
+	QWidget *root;
+	QVideoWidget *videoWidget;
 };
-
-static void layer_surface_configure(void *data,
-		struct zwlr_layer_surface_v1 *surface,
-		uint32_t serial, uint32_t w, uint32_t h) {
-	printf("resize %d  %d\n",w,h);
-	root->resize(w, h);
-	videoWidget->resize(w ,h);
-	zwlr_layer_surface_v1_ack_configure(surface, serial);
-}
-
-static void layer_surface_closed(void *data,
-		struct zwlr_layer_surface_v1 *surface) {
-	zwlr_layer_surface_v1_destroy(surface);
-	app->quit();
-}
-
-struct zwlr_layer_surface_v1_listener layer_surface_listener = {
-	.configure = layer_surface_configure,
-	.closed = layer_surface_closed,
-};
-
-void playerStateSlot(QMediaPlayer::State state){
-	if(state == QMediaPlayer::StoppedState){
-		zwlr_layer_surface_v1_destroy(layer_surface);
-		app->quit();
-	}
-}
 
 int main(int argc,char *argv[]){
+	struct wl_surface *wl_surface;
+	struct zwlr_layer_surface_v1 *layer_surface;
+	struct qvw_state state {
+		.app = new QApplication(argc, argv),
+		.root = new QWidget,
+	};
+
 	// hack: disable shell integration
 	setenv("QT_WAYLAND_SHELL_INTEGRATION","nonexistance",1);
-	app = new QApplication(argc, argv);
 	QApplication::setApplicationName("qt-video-wlr");
 	QApplication::setApplicationVersion("dev");
 
@@ -79,194 +36,201 @@ int main(int argc,char *argv[]){
 		"for wlroots based wayland compositor");
 	parser.addHelpOption();
 	parser.addVersionOption();
-	parser.addPositionalArgument("FILE","Files to play.","FILE...");
-	QCommandLineOption layerOption(QStringList() << "l" << "layer",
+	parser.addPositionalArgument("FILE", "Files to play.", "FILE...");
+	QCommandLineOption layerOption({"l", "layer"},
 		"Layer to render on, background, bottom, top or overlay."
-		" Defaults to top.", "layer");
-	QCommandLineOption widthOption(QStringList() << "w" << "width",
-		"Widget width. 0 to use max width. Defaults to 320.", "width");
-	QCommandLineOption heightOption(QStringList() << "e" << "height",
-		"Widget height. 0 to use max height. Defaults to 240.", "height");
-	QCommandLineOption colorOption(QStringList() << "c" << "color",
+		" Defaults to top.", "layer", "top");
+	QCommandLineOption widthOption({"w", "width"},
+		"Widget width. 0 for max width. Defaults to 320.", "width", "320");
+	QCommandLineOption heightOption({"e", "height"},
+		"Widget height. 0 for max height. Defaults to 240.", "height", "240");
+	QCommandLineOption colorOption({"c", "color"},
 		"Background color, QColor::setNamedColor() format.", "color");
-	QCommandLineOption volumeOption(QStringList() << "s" << "volume",
+	QCommandLineOption volumeOption({"s", "volume"},
 		"Linear sound volume, [0,100]. Defaults to 100.", "volume");
-	QCommandLineOption positionOption(QStringList() << "p" << "position",
+	QCommandLineOption positionOption({"p", "position"},
 		"Widget position: center, top, top-left, top-right, "
 		"bottom, bottom-left, bottom-right, left or right. "
-		"Defaults to bottom-right.", "position");
-	QCommandLineOption loopOption(QStringList() << "n" << "no-loop",
+		"Defaults to bottom-right.", "position", "bottom-right");
+	QCommandLineOption noLoopOption({"n", "no-loop"},
 		"Do not loop. Defaults to loop.");
-	QCommandLineOption marginOption(QStringList() << "m" << "margin",
+	QCommandLineOption marginOption({"m", "margin"},
 		"Widget margin. Edge-specific options take precedence if specified. "
-		"No effect when not sticking to the edge. Defaults to 0.","margin");
-	QCommandLineOption topMarginOption(QStringList() << "top-margin",
-		"Widget top margin. Defaults to 0.","margin");
-	QCommandLineOption rightMarginOption(QStringList() << "right-margin",
-		"Widget right margin. Defaults to 0.","margin");
-	QCommandLineOption bottomMarginOption(QStringList() << "bottom-margin",
-		"Widget bottom margin. Defaults to 0.","margin");
-	QCommandLineOption leftMarginOption(QStringList() << "left-margin",
-		"Widget left margin. Defaults to 0.","margin");
-	parser.addOption(layerOption);
-	parser.addOption(widthOption);
-	parser.addOption(heightOption);
-	parser.addOption(colorOption);
-	parser.addOption(volumeOption);
-	parser.addOption(positionOption);
-	parser.addOption(loopOption);
-	parser.addOption(marginOption);
-	parser.addOption(topMarginOption);
-	parser.addOption(rightMarginOption);
-	parser.addOption(bottomMarginOption);
-	parser.addOption(leftMarginOption);
-	parser.process(*app);
+		"No effect when not sticking to the edge. Defaults to 0.", "margin", "0");
+	QCommandLineOption topMarginOption("top-margin",
+		"Widget top margin. Defaults to 0.", "margin", "0");
+	QCommandLineOption rightMarginOption("right-margin",
+		"Widget right margin. Defaults to 0.", "margin", "0");
+	QCommandLineOption bottomMarginOption("bottom-margin",
+		"Widget bottom margin. Defaults to 0.", "margin", "0");
+	QCommandLineOption leftMarginOption("left-margin",
+		"Widget left margin. Defaults to 0.", "margin", "0");
+	parser.addOptions({ layerOption, widthOption, heightOption, colorOption,
+		volumeOption, positionOption, noLoopOption, marginOption,
+		topMarginOption, rightMarginOption, bottomMarginOption,
+		leftMarginOption });
+	parser.process(*state.app);
 
-	uint32_t layer = ZWLR_LAYER_SHELL_V1_LAYER_TOP;
-	uint32_t anchor = ZWLR_LAYER_SURFACE_V1_ANCHOR_RIGHT |
-		ZWLR_LAYER_SURFACE_V1_ANCHOR_BOTTOM;
-	int32_t top_margin = 0, right_margin = 0, bottom_margin = 0,
-		left_margin = 0;
-	if(parser.isSet(layerOption)){
-		QString str = parser.value(layerOption);
-		if(str == "background")
-			layer = ZWLR_LAYER_SHELL_V1_LAYER_BACKGROUND;
-		else if(str == "bottom")
-			layer = ZWLR_LAYER_SHELL_V1_LAYER_BOTTOM;
-		else if(str == "overlay")
-			layer = ZWLR_LAYER_SHELL_V1_LAYER_OVERLAY;
-		else if(str != "top")
-			parser.showHelp(1);
-	}
-	if(parser.isSet(widthOption)){
-		bool ok;
-		width = parser.value(widthOption).toInt(&ok, 10);
-		if(!ok||width < 0) parser.showHelp(1);
-	}
-	if(parser.isSet(heightOption)){
-		bool ok;
-		height = parser.value(heightOption).toInt(&ok, 10);
-		if(!ok||height < 0) parser.showHelp(1);
-	}
-	if(parser.isSet(positionOption)){
-		QString str = parser.value(positionOption);
-		if(str == "center")
-			anchor = 0;
-		else if(str == "top")
-			anchor = ZWLR_LAYER_SURFACE_V1_ANCHOR_TOP;
-		else if(str == "top-left")
-			anchor = ZWLR_LAYER_SURFACE_V1_ANCHOR_TOP |
-				ZWLR_LAYER_SURFACE_V1_ANCHOR_LEFT;
-		else if(str == "top-right")
-			anchor = ZWLR_LAYER_SURFACE_V1_ANCHOR_TOP |
-				ZWLR_LAYER_SURFACE_V1_ANCHOR_RIGHT;
-		else if(str == "bottom")
-			anchor = ZWLR_LAYER_SURFACE_V1_ANCHOR_BOTTOM;
-		else if(str == "bottom-left")
-			anchor = ZWLR_LAYER_SURFACE_V1_ANCHOR_BOTTOM |
-				ZWLR_LAYER_SURFACE_V1_ANCHOR_LEFT;
-		else if(str == "left")
-			anchor = ZWLR_LAYER_SURFACE_V1_ANCHOR_LEFT;
-		else if(str == "right")
-			anchor = ZWLR_LAYER_SURFACE_V1_ANCHOR_RIGHT;
-		else if(str != "bottom-right")
-			parser.showHelp(1);
-		if (width == 0)
-			anchor |= ZWLR_LAYER_SURFACE_V1_ANCHOR_RIGHT |
-				ZWLR_LAYER_SURFACE_V1_ANCHOR_LEFT;
-		if (height == 0)
-			anchor |= ZWLR_LAYER_SURFACE_V1_ANCHOR_BOTTOM |
-				ZWLR_LAYER_SURFACE_V1_ANCHOR_TOP;
-	}
-	if(parser.isSet(marginOption)){
-		bool ok;
-		top_margin = right_margin = bottom_margin = left_margin =
-			parser.value(marginOption).toInt(&ok, 10);
-		if(!ok||width < 0) parser.showHelp(1);
-	}
-	if(parser.isSet(topMarginOption)){
-		bool ok;
-		top_margin = parser.value(topMarginOption).toInt(&ok, 10);
-		if(!ok||width < 0) parser.showHelp(1);
-	}
-	if(parser.isSet(rightMarginOption)){
-		bool ok;
-		right_margin = parser.value(rightMarginOption).toInt(&ok, 10);
-		if(!ok||width < 0) parser.showHelp(1);
-	}
-	if(parser.isSet(bottomMarginOption)){
-		bool ok;
-		bottom_margin = parser.value(bottomMarginOption).toInt(&ok, 10);
-		if(!ok||width < 0) parser.showHelp(1);
-	}
-	if(parser.isSet(leftMarginOption)){
-		bool ok;
-		left_margin = parser.value(leftMarginOption).toInt(&ok, 10);
-		if(!ok||width < 0) parser.showHelp(1);
-	}
+	uint32_t layer, anchor;
+	int32_t top_margin, right_margin, bottom_margin, left_margin;
+	bool ok;
+	QString layerStr = parser.value(layerOption);
+	if (layerStr == "background")
+		layer = ZWLR_LAYER_SHELL_V1_LAYER_BACKGROUND;
+	else if (layerStr == "bottom")
+		layer = ZWLR_LAYER_SHELL_V1_LAYER_BOTTOM;
+	else if (layerStr == "top")
+		layer = ZWLR_LAYER_SHELL_V1_LAYER_TOP;
+	else if (layerStr == "overlay")
+		layer = ZWLR_LAYER_SHELL_V1_LAYER_OVERLAY;
+	else
+		parser.showHelp(1);
+
+	state.width = parser.value(widthOption).toInt(&ok);
+	if (!ok) parser.showHelp(1);
+	state.height = parser.value(heightOption).toInt(&ok);
+	if (!ok) parser.showHelp(1);
+
+	QString positionStr = parser.value(positionOption);
+	if (positionStr == "center")
+		anchor = 0;
+	else if (positionStr == "top")
+		anchor = ZWLR_LAYER_SURFACE_V1_ANCHOR_TOP;
+	else if (positionStr == "top-left")
+		anchor = ZWLR_LAYER_SURFACE_V1_ANCHOR_TOP |
+			ZWLR_LAYER_SURFACE_V1_ANCHOR_LEFT;
+	else if (positionStr == "top-right")
+		anchor = ZWLR_LAYER_SURFACE_V1_ANCHOR_TOP |
+			ZWLR_LAYER_SURFACE_V1_ANCHOR_RIGHT;
+	else if (positionStr == "bottom")
+		anchor = ZWLR_LAYER_SURFACE_V1_ANCHOR_BOTTOM;
+	else if (positionStr == "bottom-left")
+		anchor = ZWLR_LAYER_SURFACE_V1_ANCHOR_BOTTOM |
+			ZWLR_LAYER_SURFACE_V1_ANCHOR_LEFT;
+	else if (positionStr == "bottom-right")
+		anchor = ZWLR_LAYER_SURFACE_V1_ANCHOR_BOTTOM |
+			ZWLR_LAYER_SURFACE_V1_ANCHOR_RIGHT;
+	else if (positionStr == "left")
+		anchor = ZWLR_LAYER_SURFACE_V1_ANCHOR_LEFT;
+	else if (positionStr == "right")
+		anchor = ZWLR_LAYER_SURFACE_V1_ANCHOR_RIGHT;
+	else
+		parser.showHelp(1);
+	if (state.width == 0)
+		anchor |= ZWLR_LAYER_SURFACE_V1_ANCHOR_RIGHT |
+			ZWLR_LAYER_SURFACE_V1_ANCHOR_LEFT;
+	if (state.height == 0)
+		anchor |= ZWLR_LAYER_SURFACE_V1_ANCHOR_BOTTOM |
+			ZWLR_LAYER_SURFACE_V1_ANCHOR_TOP;
+
+	top_margin = right_margin = bottom_margin = left_margin =
+		parser.value(marginOption).toInt(&ok);
+	if (!ok) parser.showHelp(1);
+	top_margin = parser.value(topMarginOption).toInt(&ok);
+	if (!ok) parser.showHelp(1);
+	right_margin = parser.value(rightMarginOption).toInt(&ok);
+	if (!ok) parser.showHelp(1);
+	bottom_margin = parser.value(bottomMarginOption).toInt(&ok);
+	if (!ok) parser.showHelp(1);
+	left_margin = parser.value(leftMarginOption).toInt(&ok);
+	if (!ok) parser.showHelp(1);
 
 	QPlatformNativeInterface *native = QGuiApplication::platformNativeInterface();
 	struct wl_display *display = (struct wl_display *)
 		native->nativeResourceForWindow("display", NULL);
 	struct wl_registry *registry = wl_display_get_registry(display);
-	wl_registry_add_listener(registry, &registry_listener, NULL);
+	const struct wl_registry_listener registry_listener = {
+		.global = [](void *data, struct wl_registry *registry,
+				uint32_t name, const char *interface, uint32_t version) {
+			struct qvw_state *state = static_cast<struct qvw_state *>(data);
+			if (strcmp(interface, zwlr_layer_shell_v1_interface.name) == 0) {
+				state->layer_shell = (zwlr_layer_shell_v1*)wl_registry_bind(
+						registry, name, &zwlr_layer_shell_v1_interface, 1);
+			}
+		},
+		.global_remove = [](void *data, struct wl_registry *registry, uint32_t name) {},
+	};
+	wl_registry_add_listener(registry, &registry_listener, &state);
 	wl_display_roundtrip(display);
-	if(layer_shell == NULL){
+	if (state.layer_shell == NULL) {
 		fprintf(stderr, "layer_shell not available\n");
+		state.app->quit();
 		return 1;
 	}
 
-	auto player = new QMediaPlayer;
-	auto playlist = new QMediaPlaylist;
+	QMediaPlayer player;
+	QMediaPlaylist playlist;
 	QStringList pos_args = parser.positionalArguments();
-	if(pos_args.size() == 0){
+	if (pos_args.size() == 0)
 		parser.showHelp(1);
-	}
-	for(int a = 0;a < pos_args.size();a++)
-		playlist->addMedia(QUrl::fromLocalFile(QFileInfo(pos_args.at(a))
-			.absoluteFilePath()));
-	if(!parser.isSet(loopOption))
-		playlist->setPlaybackMode(QMediaPlaylist::Loop);
-	player->setPlaylist(playlist);
-	if(parser.isSet(volumeOption)){
-		bool ok;
-		player->setVolume(parser.value(volumeOption).toInt(&ok, 10));
-		if(!ok||height < 0) parser.showHelp(1);
+	for (auto path: pos_args)
+		playlist.addMedia(
+			QUrl::fromLocalFile(QFileInfo(path).absoluteFilePath()));
+	if (!parser.isSet(noLoopOption))
+		playlist.setPlaybackMode(QMediaPlaylist::Loop);
+	player.setPlaylist(&playlist);
+	if (parser.isSet(volumeOption)) {
+		player.setVolume(parser.value(volumeOption).toInt(&ok));
+		if (!ok) parser.showHelp(1);
 	}
 
-	root = new QWidget;
-	videoWidget = new QVideoWidget(root);
-	root->resize(width ? width : 1, height ? height : 1);
-	root->show();
+	state.videoWidget = new QVideoWidget(state.root);
+	state.root->resize(state.width ? state.width : 1, state.height ? state.height : 1);
+	state.root->show();
+
 	wl_surface = static_cast<struct wl_surface *>(
-		native->nativeResourceForWindow("surface", root->windowHandle()));
-	layer_surface = zwlr_layer_shell_v1_get_layer_surface(layer_shell,
+		native->nativeResourceForWindow("surface", state.root->windowHandle()));
+	layer_surface = zwlr_layer_shell_v1_get_layer_surface(state.layer_shell,
 		wl_surface, NULL, layer, "video-player");
+
+	const struct zwlr_layer_surface_v1_listener layer_surface_listener = {
+		.configure = [](void *data,
+				struct zwlr_layer_surface_v1 *surface,
+				uint32_t serial, uint32_t w, uint32_t h) {
+			struct qvw_state *state = static_cast<struct qvw_state *>(data);
+			printf("resize %d %d\n", w, h);
+			state->root->resize(w, h);
+			state->videoWidget->resize(w ,h);
+			zwlr_layer_surface_v1_ack_configure(surface, serial);
+		},
+		.closed = [](void *data,
+				struct zwlr_layer_surface_v1 *surface) {
+			struct qvw_state *state = static_cast<struct qvw_state *>(data);
+			zwlr_layer_surface_v1_destroy(surface);
+			state->app->quit();
+		},
+	};
 	zwlr_layer_surface_v1_set_margin(layer_surface, top_margin, right_margin,
 		bottom_margin, left_margin);
-	zwlr_layer_surface_v1_set_size(layer_surface, width, height);
+	zwlr_layer_surface_v1_set_size(layer_surface, state.width, state.height);
 	zwlr_layer_surface_v1_set_anchor(layer_surface, anchor);
-	zwlr_layer_surface_v1_add_listener(layer_surface, &layer_surface_listener, layer_surface);
+	zwlr_layer_surface_v1_add_listener(layer_surface, &layer_surface_listener, &state);
 	wl_surface_commit(wl_surface);
 	wl_display_roundtrip(display);
 
-	player->setVideoOutput(videoWidget);
-	videoWidget->setMinimumSize(width ? width : 1, height ? height : 1);
-	if(parser.isSet(colorOption)){
+	player.setVideoOutput(state.videoWidget);
+	state.videoWidget->setMinimumSize(state.width ? state.width : 1, state.height ? state.height : 1);
+	if (parser.isSet(colorOption)) {
 		QColor qcolor(parser.value(colorOption));
 		if(!qcolor.isValid()) parser.showHelp(1);
 		QPalette pal;
 		pal.setColor(QPalette::Window, qcolor);
-		videoWidget->setAutoFillBackground(true);
-		videoWidget->setPalette(pal);
+		state.videoWidget->setAutoFillBackground(true);
+		state.videoWidget->setPalette(pal);
 		QPalette pal2;
 		pal2.setColor(QPalette::Window, "#00000000");
-		root->setAutoFillBackground(true);
-		root->setPalette(pal2);
+		state.root->setAutoFillBackground(true);
+		state.root->setPalette(pal2);
 	}
-	videoWidget->show();
-	player->play();
-	QObject::connect(player, &QMediaPlayer::stateChanged, playerStateSlot);
+	state.videoWidget->show();
+	player.play();
+	QObject::connect(&player, &QMediaPlayer::stateChanged,
+		[&layer_surface, &state](QMediaPlayer::State playerState) {
+			if(playerState == QMediaPlayer::StoppedState){
+				zwlr_layer_surface_v1_destroy(layer_surface);
+				state.app->quit();
+			}
+		});
 
-	return app->exec();
+	return state.app->exec();
 }
